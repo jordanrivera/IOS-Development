1633886aa3c725fb1a142578639fa1a531c14385
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-09-01 18:20:41 +0200

    first_commit_webapp

---
+++ JRM-TechnologyServices/JRM-TechnologyServicesTests/JRM_TechnologyServicesTests.swift
@@ -0,0 +1,34 @@
+//
+//  JRM_TechnologyServicesTests.swift
+//  JRM-TechnologyServicesTests
+//
+//  Created by cex on 01/09/2020.
+//  Copyright © 2020 JRM. All rights reserved.
+//
+
+import XCTest
+@testable import JRM_TechnologyServices
+
+class JRM_TechnologyServicesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
